3eec71734f345073889bd193e69f22f1d8ddee74
Brandon Evans <bevans@customorthopaedics.com>
2017-10-21 13:42:57 -0400

    Add contests collection, get contests from database

--- imports/ui/Ballot.jsx
+++ imports/ui/Ballot.jsx
@@ -1,22 +1,24 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { createContainer } from 'meteor/react-meteor-data'
+import { Contests } from '../api/contests.js'
  
 import Contest from './Contest.jsx'
  
-// App component - represents the whole app
-export default class Ballot extends Component {
-  getContests() {
-    return [
-      { _id: 1, text: 'Rank your choices', 
-      contestType: 'rankedChoice', choices: ['YES', 'NO'] },
-      { _id: 2, text: 'Vote up or down',
-      contestType: 'simpleMajority', choices: ['up', 'down'] },
-      { _id: 3, text: 'Vote for two: Bob or Jim or write-in',
-      contestType: 'pickTwo', choices: ['Bob', 'Jim', '_'] },
-    ]
-  }
+class Ballot extends Component {
+//  getContests() {
+//    return [
+//      { _id: 1, text: 'Rank your choices', 
+ //     contestType: 'rankedChoice', choices: ['YES', 'NO'] },
+ //     { _id: 2, text: 'Vote up or down',
+ //     contestType: 'simpleMajority', choices: ['up', 'down'] },
+ //     { _id: 3, text: 'Vote for two: Bob or Jim or write-in',
+ //     contestType: 'pickTwo', choices: ['Bob', 'Jim', '_'] },
+ //   ]
+ // }
  
   renderContests() {
-    return this.getContests().map((contest) => (
+    return this.props.contests.map((contest) => (
       <Contest key={contest._id} contest={contest} />
     ))
   }
@@ -33,3 +35,13 @@ export default class Ballot extends Component {
     )
   }
 }
+
+Ballot.propTypes = {
+  contests: PropTypes.array.isRequired,
+};
+ 
+export default createContainer(() => {
+  return {
+    contests: Contests.find({}).fetch(),
+  };
+}, Ballot);
